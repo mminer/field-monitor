4e61eebaf7dea832f295de9a62368d08488f85f6
Matthew Miner <matthew@matthewminer.com>
2014-01-21 19:53:51 -0500

    Add demo file.

---
+++ Assets/Field Monitor/Demo.cs
@@ -0,0 +1,28 @@
+﻿namespace FieldMonitor
+{
+	/// <summary>
+	/// Sample class demonstrating monitored fields, properties, and classes.
+	///
+	/// Uncomment the contents of this class then open the Field Monitor window
+	/// to see it in action.
+	/// </summary>
+	class Demo
+	{
+		/*
+		static string hiddenField;
+
+		[FieldMonitor.Monitor]
+		static float monitoredField = 42;
+
+		[FieldMonitor.Monitor]
+		public static int monitoredProperty { get; set; }
+
+		[FieldMonitor.Monitor]
+		class AnotherDemo
+		{
+			static string anotherField = "Hello old sport.";
+			public static bool anotherProperty { get; set; }
+		}
+		*/
+	}
+}
---
+++ Assets/Field Monitor/Editor/MainWindow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace FieldMonitor
+{
+	/// <summary>
+	/// The primary Field Monitor editor window.
+	/// </summary>
+	class MainWindow : EditorWindow
+	{
+		static Dictionary<Type, MemberInfo[]> monitored;
+		Vector2 scrollPos;
+
+		[MenuItem("Window/Field Monitor")]
+		static void Init ()
+		{
+			EditorWindow.GetWindow<MainWindow>("Field Monitor");
+		}
+
+		void OnGUI ()
+		{
+			if (monitored == null) {
+				RefreshMonitored();
+			}
+
+			scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+			foreach (var group in monitored) {
+				GUILayout.Label(group.Key.Name, EditorStyles.boldLabel);
+
+				foreach (var member in group.Value) {
+					var displayValue = GetDisplayValue(member);
+					EditorGUILayout.LabelField(member.Name, displayValue);
+				}
+			}
+
+			EditorGUILayout.EndScrollView();
+		}
+
+		void OnFocus ()
+		{
+			RefreshMonitored();
+		}
+
+		/// <summary>
+		/// Refreshes the types that are being monitored.
+		/// </summary>
+		static void RefreshMonitored ()
+		{
+			monitored = MemberFinder.GetMonitoredMembers();
+		}
+
+		/// <summary>
+		/// Gets a displayable value of the member.
+		/// </summary>
+		/// <param name="member">Field or property.</param>
+		/// <returns>Display value.</returns>
+		static string GetDisplayValue (MemberInfo member)
+		{
+			string displayValue = "";
+			object val = null;
+
+			if (member is FieldInfo) {
+				val = (member as FieldInfo).GetValue(null);
+			} else if (member is PropertyInfo) {
+				var getMethod = (member as PropertyInfo).GetGetMethod();
+
+				if (getMethod != null) {
+					val = getMethod.Invoke(null, null);
+				}
+			}
+
+			if (val != null) {
+				displayValue += val;
+			}
+
+			return displayValue;
+		}
+	}
+}
